39c782032266575e1312f6e8ec2b29b3aa0c7d61
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-22 14:46:12 -0600

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://Christian-Horban.github.io/meet"
+  "https://Christian-Horban.github.io/meet2"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
