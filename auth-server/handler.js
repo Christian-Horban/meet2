bc51ddb67b5b61b4455f0184e7ad0d42e70ee298
Git-Xian831 <christian.k.horban@gmail.com>
2023-09-27 00:01:11 -0600

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -28,6 +28,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
+      "Access-Control-Allow-Headers": "*",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true,
     },
